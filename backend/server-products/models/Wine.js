d8706d594cbc17c40ced7a35cf8fca06b826dc45
Kostadin <k.raychev@abv.bg>
2023-12-03 11:18:22 +0200

    adding timestamps for the products

--- backend/server-products/models/Wine.js
+++ backend/server-products/models/Wine.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 
-const wineSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
-  photo: String,
-  date: { type: Date, default: Date.now },
-  qty: { type: Number, default: 0 },
-  category: String,
-});
+const wineSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    photo: String,
+    date: { type: Date, default: Date.now },
+    qty: { type: Number, default: 0 },
+    category: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Wine = mongoose.model("Wine", wineSchema);
 module.exports = Wine;

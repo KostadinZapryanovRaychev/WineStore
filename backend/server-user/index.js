96e109390668c3b45de149f3232455b55f35e365
Kostadin <k.raychev@abv.bg>
2023-12-03 11:16:57 +0200

    adding user model , adding validation , creating userControllers and index.js server for user paths

---
+++ backend/server-user/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userController = require("./controllers/userController");
+
+const app = express();
+const port = 5002;
+
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/users", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("connected", async () => {
+  console.log("Mongoose connected to Users database");
+});
+
+db.on("error", (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+});
+
+db.on("disconnected", () => {
+  console.log("Mongoose disconnected from Users database");
+});
+
+app.get("/users", userController.getUsers);
+app.post("/users", userController.registerUser);
+app.get("/users/:id", userController.getUserById);
+app.put("/users/:id", userController.updateUser);
+app.delete("/users/:id", userController.deleteUser);
+app.post("/login", userController.loginUser);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ backend/server-user/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    firstName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    birthDate: {
+      type: Date,
+    },
+    registeredDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

b6d15fbd218b16f6b482f240a840bb816e7a795f
Kostadin <k.raychev@abv.bg>
2023-12-03 11:29:37 +0200

    updating model of Wine

--- backend/server-products/models/Wine.js
+++ backend/server-products/models/Wine.js
@@ -6,7 +6,6 @@ const wineSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     description: { type: String, required: true },
     photo: String,
-    date: { type: Date, default: Date.now },
     qty: { type: Number, default: 0 },
     category: String,
   },

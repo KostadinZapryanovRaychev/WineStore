de18f9d1c3becc0291cbb8aa67566f61aa3fb96e
Kostadin <k.raychev@abv.bg>
2023-12-06 00:11:27 +0200

    fixing Footer styles

--- frontend/footer/src/Footer.jsx
+++ frontend/footer/src/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer-container">Footer</div>;
+  return <div className="footer-container">Super Fancy Footer</div>;
 }
 
 export default Footer;
